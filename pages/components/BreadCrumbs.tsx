10cddf60e4faca7fda1c83fcaa4e7a6ec3c65e0b
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-11-18 13:50:01 +0500

    breadCrumbs reinitialized

--- pages/components/BreadCrumbs.tsx
+++ pages/components/BreadCrumbs.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const BreadCrumbs = ({crumbs_title, URL}:any) => {
   return (
     <div className='flex items-center justify-between bg-[#fff] border-l-[5px] border-[#a73263] p-[8px] rounded-sm'>
-      <b>{crumbs_title}</b>
-      <Link href={URL}>Read More</Link>
+      <b>{`${crumbs_title}`}</b>
+      <Link href={`${URL}`}>Read More</Link>
     </div>
   )
 }
